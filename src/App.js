69e3adc31716eb9a9ef7de2d81c9625dd8cb5e37
Cecile Alfred <cecile.alfredx@gmail.com>
2017-11-28 23:52:07 +0000

    Initial commit with React

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react app!</h1>
+        <p>This is really working.</p>
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'is it working?'));
+  }
+}
+
+export default App;
+
+{/* This is a comment on a JS / JSX file
+  For each .js file, go to
+  view > syntax > open all > babel > JS (react)
+*/}
