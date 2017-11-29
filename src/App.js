ffeb388ba8601874d1fad85a76590cff111c945a
Cecile Alfred <cecile.alfredx@gmail.com>
2017-11-29 00:52:08 +0000

    Created and changed a state with a button click

--- src/App.js
+++ src/App.js
@@ -3,13 +3,35 @@ import './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
+  // State is only available in components which extend
+  state = {
+    persons: [
+      { name: 'Cecile', age: 25 },
+      { name: 'Jeanne', age: 28 },
+      { name: 'Leon', age: 23 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was clicked');
+    this.setState( {
+      persons: [
+      { name: 'Cecile', age: 25 },
+      { name: 'Jeanne', age: 28 },
+      { name: 'Leonard', age: 23 }
+      ]
+    })
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a react app!</h1>
         <p>This is really working.</p>
-        <Person name="Cecile" age="25" />
-        <Person name="Jeanne" age="28">My hobbies: Painting.</Person>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Painting.</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'is it working?'));
