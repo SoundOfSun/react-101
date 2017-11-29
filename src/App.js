220f12115d42ff48038d7eebd878bf667fe96313
Cecile Alfred <cecile.alfredx@gmail.com>
2017-11-29 12:17:52 +0000

    Experimented with method references between components

--- src/App.js
+++ src/App.js
@@ -8,15 +8,15 @@ class App extends Component {
     persons: [
       { name: 'Cecile', age: 25 },
       { name: 'Jeanne', age: 28 },
-      { name: 'Leon', age: 23 }
+      { name: 'Leon', age: 24 }
     ]
   }
 
-  switchNameHandler = () => {
+  switchNameHandler = (newName) => {
     // console.log('Was clicked');
     this.setState( {
       persons: [
-      { name: 'Cecile', age: 25 },
+      { name: newName, age: 25 },
       { name: 'Jeanne', age: 28 },
       { name: 'Leonard', age: 23 }
       ]
@@ -28,10 +28,14 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm a react app!</h1>
         <p>This is really working.</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Painting.</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={() => this.switchNameHandler('Cecilia!!')}>Switch Name</button>
+        <Person name={this.state.persons[0].name}
+        age={this.state.persons[0].age}
+        click={this.switchNameHandler.bind(this, 'Cecilia')} />
+        <Person name={this.state.persons[1].name}
+        age={this.state.persons[1].age}>My hobbies: Painting.</Person>
+        <Person name={this.state.persons[2].name}
+        age={this.state.persons[2].age} />
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'is it working?'));
