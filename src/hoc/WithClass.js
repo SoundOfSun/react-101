031387549c3d6eabb412e93c47182cee4297a752
Cecile Alfred <cecile.alfredx@gmail.com>
2017-12-01 09:41:39 +0000

    Same HOC approach but by passing a stateful component

--- src/hoc/WithClass.js
+++ src/hoc/WithClass.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// const withClass = (WrappedComponent, className) => {
+//   // simple JS function with returns a functional component
+//   // could return a stateful component too
+//   return (props) => (
+//     <div className={className}>
+//       <WrappedComponent {...props} />
+//     </div>
+//   )
+// }
 
 const withClass = (WrappedComponent, className) => {
-  // simple JS function with returns a functional component
-  // could return a stateful component too
-  return (props) => (
-    <div className={className}>
-      <WrappedComponent {...props} />
-    </div>
-  )
+  // return anonymous class
+  return class extends Component {
+    render () {
+      return (
+        <div className={className}>
+          <WrappedComponent {...this.props} />
+        </div>
+      )
+    }
+  }
 }
 
 export default withClass;
