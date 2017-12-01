d5b4acdcbc057e89b4129f366fb0c2cc716ef830
Cecile Alfred <cecile.alfredx@gmail.com>
2017-12-01 00:07:13 +0000

    Update LifeCycle triggered by a State change

--- src/containers/App.js
+++ src/containers/App.js
@@ -28,6 +28,20 @@ class App extends Component {
     console.log('[App.js] Inside componentDidMount');
   }
 
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside shouldComponentUpdate', nextProps, nextState);
+    // Immutable approach of udpating persons array
+    return true;
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] Inside componentWillUpdate', nextProps, nextState);
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] Inside componentDidUpdate');
+  }
+
   // State is only available in components which extend
   // state = {
   //   persons: [
