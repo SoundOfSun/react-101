9ae1a4db1afb2287cfd8f9c509384ea5f6407539
Cecile Alfred <cecile.alfredx@gmail.com>
2017-12-01 09:49:31 +0000

    Used setState correctyly with a counter - avoid this.state inside a setState

--- src/containers/App.js
+++ src/containers/App.js
@@ -18,7 +18,8 @@ class App extends PureComponent {
         { id: '2', name: 'Jeanne', age: 28 },
         { id: '3', name: 'Leon', age: 24 }
       ],
-      showPersons: false
+      showPersons: false,
+      toggleClicked: 0
     };
   }
 
@@ -90,7 +91,13 @@ class App extends PureComponent {
 
   togglePersonsHandler = () => {
     const doesShow = this.state.showPersons;
-    this.setState({showPersons: !doesShow});
+    // to avoid using this.state in setState, use :
+    this.setState( (prevState, props) => {
+      return {
+        showPersons: !doesShow,
+        toggleClicked: prevState.toggleClicked + 1
+      }
+    });
   }
 
   render() {
